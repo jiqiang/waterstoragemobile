c14174aee992488e2e2940a68efaca14e041aa16
Simon Borysiewicz <s.borysiewicz@bom.gov.au>
2016-02-16 13:44:05 +1100

    Adjusting the env.php file to point to the include directory

--- server/include/env.php
+++ server/include/env.php
@@ -2,7 +2,7 @@
 
 interface Env {
 
-    const ENV_FILE = "env/env-current.php";
+    const ENV_FILE = "include/env-current.php";
     const ENV_DEV_FILE = "env/env-dev.php";
     const ENV_LOCAL_FILE = "env/env-local.php";
 
